6656669d3d1d1ba27b1caa971c6dbc7e50abf9a3
jelizondog13 <137620840+jelizondog13@users.noreply.github.com>
2023-06-25 04:07:38 +0200

    creandomiweb

---
+++ index.js
@@ -0,0 +1,9 @@
+const btnCart = document.querySelector('.container-icon')
+const containerCartProducts=document.querySelector('.container-cart-Products')
+
+btnCart.addEventListener('click',()=>{
+    containerCartProducts.classList.toggle
+    ('hidden-cart')
+})
+
+
